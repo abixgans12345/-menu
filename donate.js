8f7a6a94d8bade48afb87f39b9d0343f7c33b788
abixgans12345 <74518961+abixgans12345@users.noreply.github.com>
2020-11-16 20:44:42 +0700

    Add files via upload

---
+++ donate.js
@@ -0,0 +1,42 @@
+exports.donate = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagramlu, whatsapplu, kapanbotaktif, grupch1, grupch2) => {
+	return `🔰 -----[ *MENU DONASI ${BotName}* ]----- 🔰
+  
+Hi, *${id.split("@s.whatsapp.net")[0]}* 👋️
+Mau donasi? 
+
+⚠️ *${tampilTanggal}*
+⚠️ *${tampilWaktu}*
+
+♻ Silahkan donasi dibawah ini :
+   
+⚜ *OVO*: 0895-2325-8649
+⚜ *DANA*: 0895-2325-8649
+⚜ *PULSA*: 0895-2325-8649
+⚜ *GOPAY*: 0895-2325-8649
+
+📺 *Iklan* :
+
+✅ Follow akun instagram admin ${instagramlu}
+
+⚠️ INFORMASI COVID-19 TERBARU!
+
+⚠️ POSITIF: *${corohelp.confirmed.value}*
+⚠️ SEMBUH: *${corohelp.recovered.value}*
+⚠️ MENINGGAL: *${corohelp.deaths.value}*
+⚠️ UPDATE: *${corohelp.lastUpdate}*
+
+♻️ _TETAP JAGA KESEHATAN DAN SELALU PAKAI MASKER!_
+
+♻️ Mau pasang iklan di *${BotName} ?*
+☎️ WA : *${whatsapplu}*
+  
+⚠️ Gunakan dengan bijak ‼️
+⚠️ Bot ini berjalan ${kapanbotaktif} ‼️
+
+✳️ Official Grub [1] : ${grupch1}
+
+✳️ Official Grub [2] : ${grupch2}
+
+  
+🔰 -----[ *POWERED BY ${BotName}* ]----- 🔰`
+}
---
+++ info.js
@@ -0,0 +1,48 @@
+exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagramlu, whatsapplu, kapanbotaktif, grupch1, grupch2) => {
+	return `🔰 -----[ *MENU ${BotName}* ]----- 🔰
+  
+Hi, *${id.split("@s.whatsapp.net")[0]}* 👋️
+Berikut adalah info pada bot ini!✨
+
+[ ️ *${tampilTanggal}* ]
+[️ *${tampilWaktu}* ]
+
+🔴 INFO BOT! :
+
+=> *AUTHOR*: ./MrG3P5
+=> *YOUTUBE*: X - MrG3P5
+
+🔴 INFO LAIN! :
+
+OH IYA SCRIPT INI 100% GRATIS KOK.
+SCRIPT DIBUAT DENGAN BAHASA PEMROGRAMAN NODE.JS
+
+♻️ JANGAN  LUPA DONASI AGAR BOT AKTIF TERUS!
+♻️ MAU DONASI? SILAHKAN KETIK #donate
+
+📺 *Iklan* :
+
+✅ Follow akun instagram admin ${instagramlu}
+
+⚠️ INFORMASI COVID-19 TERBARU!
+
+⚠️ POSITIF: *${corohelp.confirmed.value}*
+⚠️ SEMBUH: *${corohelp.recovered.value}*
+⚠️ MENINGGAL: *${corohelp.deaths.value}*
+⚠️ UPDATE: *${corohelp.lastUpdate}*
+
+♻️ _TETAP JAGA KESEHATAN DAN SELALU PAKAI MASKER!_
+
+♻️ Mau pasang iklan di *${BotName} ?*
+☎️ WA : *${whatsapplu}*
+  
+⚠️ Gunakan dengan bijak ‼️
+⚠️ Bot ini berjalan ${kapanbotaktif} ‼️
+
+✅Official Grub [1] : ${grupch1}
+
+✅Official Grub [2] : ${grupch2}
+
+  
+🔰 -----[ *POWERED BY ${BotName}* ]----- 🔰`
+}
